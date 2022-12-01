eeb31b0c5140bdcb0ce47724f45089a4cff8ee65
Igor <igor.xot@mail.ru>
2022-12-01 22:45:38 +0300

    третья задача

--- Program.cs
+++ Program.cs
@@ -9,11 +9,18 @@ Console.WriteLine(username);
 
 // 2. Задача.
 
-Console.Write("Как тебя зовут? ");
+/*Console.Write("Как тебя зовут? ");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(username);
+*/
 
+// 3. Задача Написать программу сложения двух чисел.
+/*
+int numberA = 36, numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
 
 
 
