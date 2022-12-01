355945fa3e5187f8f3e1f3d5fb1676b77bcabe53
Igor <igor.xot@mail.ru>
2022-12-01 22:29:37 +0300

    вторая задача

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ Console.WriteLine("Привет, ");
 Console.WriteLine(username);
 */
 
+// 2. Задача.
+
+Console.Write("Как тебя зовут? ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
 
 
 
