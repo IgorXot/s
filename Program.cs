c3aa933c13bb0b3798381bc471510564f486368f
Igor <igor.xot@mail.ru>
2022-12-04 18:52:02 +0300

    семинар1 задачча 2

--- Program.cs
+++ Program.cs
@@ -22,6 +22,11 @@ int result = numberA + numberB;
 Console.WriteLine(result);
 */
 
+
+// ------------------------Seminar 1------------------------
+
+// Задача 1 Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
 Console.Write("Input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -33,3 +38,23 @@ if(num1 == guad)
     Console.Write ("Yes! ");
 else
     Console.Write ("No! ");    
+*/
+
+//Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input an integer number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+
+
+
+
+
+
